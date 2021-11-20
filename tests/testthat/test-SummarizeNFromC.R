3a55b4dc6a72d3388508fd0606ba6b9d864ab616
Natalie Butler <nataliebutler897@gmail.com>
2021-11-19 16:20:09 -0800

    Additonal test added.

--- tests/testthat/test-SummarizeNFromC.R
+++ tests/testthat/test-SummarizeNFromC.R
@@ -9,3 +9,9 @@ test_that("Object type 'C' must be character class",{expect_error( summarize_N_f
 test_that("Check that output is of expected type", {expect_s3_class(summarize_N_from_C(datateachr::vancouver_trees, diameter, cultivar_name ), c("grouped_df", "tbl_df", "tbl","data.frame"))
 }
 )
+
+
+test_that("Check that the function must have a dataframe ",{expect_true(is.data.frame((datateachr::vancouver_trees )))
+}
+)
+
